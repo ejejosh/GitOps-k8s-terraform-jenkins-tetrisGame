86890c895ad42107876b7743848361b82b6bc89f
Eje Joshua <eje_joshua@yahoo.com>
2024-03-30 20:31:51 +0100

    modified ec2 main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-080e1f13689e07408"
   instance_type          = "t2.medium"
   key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
